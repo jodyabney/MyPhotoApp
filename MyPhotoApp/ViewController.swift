13e0b56c893b47151ef9075193c175246d8131ad
Jody Abney <jodyabney@gmail.com>
2020-07-11 14:35:29 -0400

    Initial Commit

---
+++ MyPhotoApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MyPhotoApp
+//
+//  Created by Jody Abney on 7/11/20.
+//  Copyright © 2020 AbneyAnalytics. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
